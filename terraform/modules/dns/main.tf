f5eafeede34e541b38376b4a078878ec49123a35
Md Sarowar Alam <sarowar@vml.com>
2026-01-22 14:25:59 +0600

    alb

--- terraform/modules/dns/main.tf
+++ terraform/modules/dns/main.tf
@@ -8,7 +8,7 @@ resource "aws_route53_record" "main" {
   type    = "A"
 
   alias {
-    name                   = var.alb_dns_name
+    name                   = "dualstack.${var.alb_dns_name}"
     zone_id                = var.alb_zone_id
     evaluate_target_health = true
   }
